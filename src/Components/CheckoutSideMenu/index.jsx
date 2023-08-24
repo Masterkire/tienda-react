3f201822550862d19977f5c0625bd493c00935db
Master Erik <davidmasterkire@gmail.com>
2023-08-24 16:18:59 -0600

    Agregando OrderCard al aside y modificando boton de checked en card de home

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,11 +1,11 @@
 import { useContext } from 'react'
 import { ShoppingCartContext } from '../../Context'
 import { XMarkIcon } from '@heroicons/react/24/outline'
+import OrderCard from '../OrderCard'
 import './styles.css'
 
 const CheckoutSideMenu = () => {
     const context = useContext(ShoppingCartContext)
-    console.log('PRODUCT TO SHOW', context.productToShow);
 
     return ( 
         <aside className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex flex-col fixed right-0 border border-black rounded-lg bg-white`}>
@@ -15,7 +15,18 @@ const CheckoutSideMenu = () => {
                     <XMarkIcon className="h-6 w-6 text-black cursor-pointer"/>
                 </div>
             </div>
-            
+            <div className='px-6 overflow-y-scroll'>
+                {
+                    context.cartProducts.map(product => (
+                        <OrderCard 
+                            key={product.id}
+                            title={product.title}
+                            imageUrl={product.image}
+                            price={product.price}
+                        />
+                    ))
+                }
+            </div>
         </aside>
      );
 }
