389f53c4f707ce81fa0846a1ea4e421ac65fc377
Master Erik <davidmasterkire@gmail.com>
2023-09-02 17:21:53 -0600

    Agregando las OrdersCard en MyOrders

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -1,4 +1,7 @@
 import { useContext } from "react"
+import { Link } from 'react-router-dom'
+import { ChevronLeftIcon } from '@heroicons/react/24/solid'
+
 import { ShoppingCartContext } from "../../Context"
 import Layout from "../../Components/Layout"
 import OrderCard from "../../Components/OrderCard"
@@ -9,7 +12,12 @@ function MyOrder() {
     return (
       <>
         <Layout>
-          MyOrder
+        <div className='flex relative items-center justify-center w-80 mb-6'>
+            <Link to={'/my-orders'} className='absolute left-0'>
+              <ChevronLeftIcon className='h-6 w-6 text-black text-sm cursor-pointer'/>
+            </Link>
+            <h1>My Order</h1>
+          </div>
           <div className='flex flex-col w-80'>
                 {
                   context.order?.slice(-1)[0].products.map(product => (
